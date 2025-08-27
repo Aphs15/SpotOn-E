64963c7966ce084df6220d582220fde926acfffa
Aphiwe Khanyile <aphiwekanyile12@gmail.com>
2025-08-27 13:38:06 +0000

    can i publish this app as a demo?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAs9vGaHWgHO3LCLK6mUMjkI_JYS9vsfDY",
-  authDomain: "event-hopper-298iu.firebaseapp.com",
-  projectId: "event-hopper-298iu",
-  storageBucket: "event-hopper-298iu.firebasestorage.app",
-  messagingSenderId: "30468776041",
-  appId: "1:30468776041:web:e8916e14214b930630c2f8",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
